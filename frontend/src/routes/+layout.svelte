9216b815baaf23d0dba8dcbea15218c97a7d84b5
thiloho <123883702+thiloho@users.noreply.github.com>
2023-10-23 12:51:30 +0200

    Add general layout for primary route

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+    import "../app.css";
+    import { page } from "$app/stores";
+</script>
+
+<header class="p-4">
+    <nav>
+        <ul class="flex gap-4">
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/settings">Settings</a>
+            </li>
+        </ul>
+    </nav>
+    <h1 class="text-2xl">{$page.url.pathname}</h1>
+</header>
+<main class="p-4">
+    <slot />
+</main>
