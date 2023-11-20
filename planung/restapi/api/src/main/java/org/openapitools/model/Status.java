2e7ebfc70f8f1a80ee85a2c71ebecb98a54b4ef2
JUG <JGarb@Gauselmann.de>
2023-11-20 12:17:10 +0100

    implemented status service

--- planung/restapi/api/src/main/java/org/openapitools/model/Status.java
+++ planung/restapi/api/src/main/java/org/openapitools/model/Status.java
@@ -1,28 +1,40 @@
 package org.openapitools.model;
 
-import java.net.URI;
-import java.util.Objects;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import org.openapitools.jackson.nullable.JsonNullable;
-import java.time.OffsetDateTime;
-import javax.validation.Valid;
-import javax.validation.constraints.*;
 import io.swagger.v3.oas.annotations.media.Schema;
 
-
-import java.util.*;
 import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import java.util.Objects;
 
 /**
  * Status
  */
 
+@Entity
+@NamedQueries(
+        @NamedQuery(
+                name=Status.FIND_ALL,
+                query = "FROM Status")
+)
 @Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-11-19T14:12:38.522563+01:00[Europe/Berlin]")
 public class Status {
 
+  public static final String FIND_ALL="status.findall";
+
+  @Id
+  @GeneratedValue(strategy= GenerationType.IDENTITY)
+  @Column
   private Long id;
 
+
+  @Column
   private String name;
 
   public Status id(Long id) {
